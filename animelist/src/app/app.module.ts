3b7e197d8640cf989fc9e889a604548268959726
Brandon Nguyen <rmpumaz@gmail.com>
2022-01-27 12:58:20 -0500

    Add navbar component and style

--- animelist/src/app/app.module.ts
+++ animelist/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AnimeComponent } from './anime/anime.component';
+import { MangaComponent } from './manga/manga.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    AnimeComponent,
+    MangaComponent
   ],
   imports: [
     BrowserModule
